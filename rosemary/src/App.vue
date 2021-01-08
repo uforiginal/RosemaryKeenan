12e9ae1867575b004cccb3aa12a3f26c454fe4aa
Rosemary Keenan <rosemary.lillian.keenan@gmail.com>
2021-01-08 12:35:27 -0600

    added cards
    The buttons on these cards will call modals with my components in them

--- rosemary/src/App.vue
+++ rosemary/src/App.vue
@@ -1,24 +1,54 @@
 <template>
   <div id="app">
     <img alt="Vue logo" src="./assets/logo.png">
-    <!--<HelloWorld msg="Welcome to Your Vue.js App"/>-->
-    <!--<About/>--->
-    <Resume/>
+    <table>
+      <tr>
+        <td>
+          <b-card
+          title="Work Expirience"
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+          >
+          <b-card-text>View my past positions</b-card-text>
+          <b-button href="#" variant="primary"></b-button>
+          </b-card>
+        </td>
+        <td>
+          <b-card
+          title="Community Work"
+          tag="article"
+          style="max-width: 20rem;"
+          class="mb-2"
+          >
+          <b-card-text>See how I give back to my community</b-card-text>
+          <b-button href="#" variant="primary"></b-button>
+          </b-card>
+        </td>
+        <td>
+          <b-card
+            title="About Me"
+            tag="article"
+            style="max-width: 20rem;"
+            class="mb-2"
+            >
+          <b-card-text>A brief history of Rosemary Keenan</b-card-text>
+          <b-button href="#" variant="primary"></b-button>
+          </b-card>
+        </td>
+      </tr>
+    </table>
+    
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-import About from './components/About.vue'
-import Resume from './components/Resume.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+//import About from './components/About.vue'
+//import Resume from './components/Resume.vue'
 
 export default {
-  name: 'App',
-  components: {
-    HelloWorld, 
-    About,
-    Resume
-  }
+  name: 'App'
 }
 </script>
 
@@ -30,5 +60,10 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+table{
+  position: center;
+  margin-left: auto;
+  margin-right: auto;
 }
 </style>
