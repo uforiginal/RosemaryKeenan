4ddb850598773f1b16091ff527b83468c2d03b6a
Rosemary Keenan <rosemary.lillian.keenan@gmail.com>
2021-01-18 13:51:29 -0600

    did some more style stuff
    Got content centered and started on the billboard

--- rosemary/src/App.vue
+++ rosemary/src/App.vue
@@ -1,44 +1,50 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <table>
-      <tr>
-        <td>
-          <b-card
+    <div id="billboard">
+      <img src="./assets/me-professional.jpg" id="avatar"/>
+    </div>
+    <div id="content">
+      <b-card-group deck align="center">
+        <b-card
           title="Work Expirience"
           tag="article"
-          style="max-width: 20rem;"
-          class="mb-2"
+          style="max-width: 20rem; background-color: #ffe6e6;"
+          class="mb-3"
           >
-          <b-card-text>View my past positions</b-card-text>
-          <b-button href="#" variant="primary"></b-button>
-          </b-card>
-        </td>
-        <td>
-          <b-card
+          <b-card-text>View my past positions at various organizations</b-card-text>
+          <b-button style="background-color: rosybrown;"><b-icon icon="star-fill"></b-icon></b-button>
+        </b-card>
+        <b-card
           title="Community Work"
           tag="article"
-          style="max-width: 20rem;"
-          class="mb-2"
+          style="max-width: 20rem; background-color: #ffe6e6;"
+          class="mb-3"
           >
           <b-card-text>See how I give back to my community</b-card-text>
-          <b-button href="#" variant="primary"></b-button>
-          </b-card>
-        </td>
-        <td>
-          <b-card
-            title="About Me"
+          <b-button style="background-color: rosybrown;"><b-icon icon="star-fill"></b-icon></b-button>
+        </b-card>
+      </b-card-group>
+      <b-card-group deck>
+        <b-card
+            title="Education"
             tag="article"
-            style="max-width: 20rem;"
-            class="mb-2"
+            style="max-width: 20rem; background-color: #ffe6e6;"
+            class="mb-3"
             >
-          <b-card-text>A brief history of Rosemary Keenan</b-card-text>
-          <b-button href="#" variant="primary"></b-button>
-          </b-card>
-        </td>
-      </tr>
-    </table>
-    
+          <b-card-text>A brief history of my educational background</b-card-text>
+          <b-button style="background-color: rosybrown;"><b-icon icon="star-fill"></b-icon></b-button>
+        </b-card>
+        <b-card
+            title="Acknowledgements"
+            tag="article"
+            style="max-width: 20rem; background-color: #ffe6e6;"
+            class="mb-3"
+            >
+          <b-card-text>Click here to view some of the generous awards and acknowledgements I've recieved</b-card-text>
+          <b-button style="background-color: rosybrown;"><b-icon icon="star-fill"></b-icon></b-button>
+        </b-card>
+      </b-card-group>
+    </div>
   </div>
 </template>
 
@@ -59,11 +65,34 @@ export default {
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background-color: bisque;
 }
-table{
-  position: center;
-  margin-left: auto;
-  margin-right: auto;
+
+#billboard{
+  margin-bottom: 5%;
+  width: 100%;
+  background-image: url("./assets/BeBrilliantNeon.jpg");
+  background-position: center;
+  background-repeat: no-repeat;
+  background-size: 80% 100%;
+}
+
+#content{
+  margin-left:25%;
+  width: 50%;
+}
+
+#avatar{
+  border-radius: 80%;
+  border-style: double;
+  width: 200px;
+  height: auto;
+  object-fit: cover; 
+  margin-right: 80%;
+  margin-top: 10%; 
+}
+
+b-card-group {
+  margin: 0 auto;
 }
 </style>
